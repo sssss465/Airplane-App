b9d4720d682bd3023c88513c40e0343d013fbf8c
grapte <feng.chiu@nyu.edu>
2018-12-05 21:41:46 +0800

    Feature finished: Customer - 7.Track My Spending

--- routes/customer.js
+++ routes/customer.js
@@ -16,25 +16,40 @@ router.get('/flights', (req, res, next) => {
 
 router.get('/spending', (req, res, next) => {
   // view total spending
-  console.log('req.body: ', req.body);
   connection.query(
-    'select sum(price) as price\n' +
-    'from (purchases natural join ticket) natural join flight\n' +
-    'where customer_email = ? and\n' +
-    '  purchase_date >= CURDATE() - interval 1 year',
+    "select date_format(purchase_date, '%Y-%m') `date`, sum(price) value\n" +
+    "from (purchases natural join ticket) natural join flight\n" +
+    "where customer_email = ? and\n" +
+    "  purchase_date <= curdate() and\n" +
+    "  purchase_date >= curdate() - interval 6 month\n" +
+    "group by date_format(purchase_date, '%Y-%m'), month(purchase_date)\n" +
+    "order by `date`",
     [req.user.email], (error, results, fields) => {
       if (error) {
         throw error;
       }
-      const price = (results[0].price === null) ? 0 : results[0].price;
-      res.render('spending', {user: req.user, sum_str_msg: "Your spending is " + price + " in the past year"});
+      const data = JSON.stringify(results);
+
+      connection.query(
+        'select sum(price) as price\n' +
+        'from (purchases natural join ticket) natural join flight\n' +
+        'where customer_email = ? and\n' +
+        '  purchase_date >= CURDATE() - interval 1 year',
+        [req.user.email], (error, results, fields) => {
+          if (error) {
+            throw error;
+          }
+          const price = (results[0].price === null) ? 0 : results[0].price;
+          res.render('spending', {
+            user: req.user,
+            data: data,
+            sum_str_msg: "Your spending is " + price + " in the past year"
+          });
+        });
     });
 });
 
 router.post('/spending', (req, res, next) => {
-  console.log('req.body: ', req.body);
-  let data = JSON.stringify([{a: 1, b: 2}, {a: 3, b: 2}, {a: 2, b: 4}]);
-  let price = null;
   connection.query(
     "select date_format(purchase_date, '%Y-%m') `date`, sum(price) value\n" +
     "from (purchases natural join ticket) natural join flight\n" +
@@ -47,33 +62,30 @@ router.post('/spending', (req, res, next) => {
       if (error) {
         throw error;
       }
-      console.log('results: ', results);
 
       // const data = results.map(d => {
       //   return {date: d.date, value: d.value};
       // });
-      data = JSON.stringify(results);
-      console.log('data: ', data);
-    });
+      const data = JSON.stringify(results);
 
-  connection.query(
-    'select sum(price) as price\n' +
-    'from (purchases natural join ticket) natural join flight\n' +
-    'where customer_email = ? and\n' +
-    '  purchase_date >= ? and\n' +
-    '  purchase_date <= ?',
-    [req.user.email, req.body.begin, req.body.end], (error, results, fields) => {
-      if (error) {
-        throw error;
-      }
-      price = (results[0].price === null) ? 0 : results[0].price;
+      connection.query(
+        'select sum(price) as price\n' +
+        'from (purchases natural join ticket) natural join flight\n' +
+        'where customer_email = ? and\n' +
+        '  purchase_date >= ? and\n' +
+        '  purchase_date <= ?',
+        [req.user.email, req.body.begin, req.body.end], (error, results, fields) => {
+          if (error) {
+            throw error;
+          }
+          const price = (results[0].price === null) ? 0 : results[0].price;
+          res.render('spending', {
+            user: req.user,
+            data: data,
+            sum_str_msg: "Your spending is " + price + " between " + req.body.begin + " and " + req.body.end
+          });
+        });
     });
-
-  res.render('spending', {
-    user: req.user,
-    data: data,
-    sum_str_msg: "Your spending is " + price + " between " + req.body.begin + " and " + req.body.end
-  });
 });
 
 module.exports = router;
