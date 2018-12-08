4e9749cf33698fd5b4f85917151e6d21125a4ad9
Andrew <sssss465@gmail.com>
2018-12-08 19:32:06 +0800

    add plane works

--- routes/staff.js
+++ routes/staff.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../db');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.render('splash', { user : req.user,
-    action: {"flights": "View your booked customer flights",
-                                  "commission": "View your commission",
-                                'topcustomers': "View top five customers based on ticket quantity and ticket sales"}});
+  res.render('splash', {
+    user: req.user,
+    action: {
+      "createflight": "Create new flights",
+      "createplane": "Create new planes",
+      'createairport': "Create new airports",
+      'viewbookingagents': "Create new airports",
+      'viewcustomers': "Create new airports",
+      'revenue': "Create new airports",
+      'topdestinations': "Create new airports",
+    }
+  });
 });
 
 router.get('/createflight', (req, res, next) => {
@@ -16,10 +25,23 @@ router.post('/createflight', (req, res, next) => {
 
 });
 router.get('/createplane', (req, res, next) => {
-
+  connection.query('select * from airplane where airline_name=?', [req.user.airline_name], (err, results, fields) => {
+    if (err) {
+      throw err;
+    }
+    res.render('createplane', {
+      user: req.user,
+      results: results
+    });
+  });
 });
 router.post('/createplane', (req, res, next) => {
-
+  connection.query('insert into airplane values(?,?,?)', [req.body.airline_name, req.body.airplane_id, req.body.seats], (err, results, fields) => {
+    if (err) {
+      throw err;
+    }
+    res.redirect('createplane');
+  });
 });
 router.get('/createairport', (req, res, next) => {
 
